bd5353af8308f38dac42978495d4163441096469
Michele Maione <mikymaione@hotmail.it>
2021-01-04 14:47:46 +0100

    Mame version compila

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "TCPServer.hpp"
+#include "server_ws_impl.hpp"
 
 void elabora()
 {
@@ -7,26 +7,8 @@ void elabora()
 
 int main()
 {
-    printf("FM_Server - Maione Miky\n");
-
-    int porta = 50000; //default
-
-    if (porta >= 49152 && porta <= 65535)
-    {
-        int IDConnessioneInAscolto = -1;
-        printf("Creazione server su porta %i\n", porta);
-
-        TCPServer tcpServer;
-        tcpServer.TCP_Crea(porta, &IDConnessioneInAscolto);
-
-        if (IDConnessioneInAscolto >= 0)
-        {
-            printf("Server connesso! ID: %i\n", IDConnessioneInAscolto);
-            printf("Server in ascolto su porta %i\n", porta);
-
-            tcpServer.TCP_Ascolta(IDConnessioneInAscolto, elabora);
-        }
-    }
+    webpp::ws_server wsServer;
+    wsServer.start();
 
     return 0;
 }
