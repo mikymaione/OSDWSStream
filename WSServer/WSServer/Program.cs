a7824bcaa2c3f9b209a84d6683be56c988c61f39
mikymaione <mikymaione@hotmail.it>
2021-02-06 20:39:22 +0100

    ok canvas c#

---
+++ WSServer/WSServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+
+namespace WSServer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var sockets = new List<IWebSocketConnection>();
+            var server = new WebSocketServer("ws://127.0.0.1:8888/");
+            server.ListenerSocket.NoDelay = true;
+            server.RestartAfterListenError = true;
+
+            server.Start(conn =>
+            {
+                conn.OnOpen = () =>
+                {
+                    Console.WriteLine("Connection open.");
+                    sockets.Add(conn);
+                };
+
+                conn.OnClose = () =>
+                {
+                    Console.WriteLine("Connection closed.");
+                    sockets.Remove(conn);
+                };
+
+                /*
+                conn.OnMessage = message =>
+                {
+                    Console.WriteLine("Client Says: " + message);
+                    sockets.ForEach(s => s.Send(" client says: " + message));
+                };
+                */
+            });
+
+            var img = System.IO.File.ReadAllBytes("img.jpg");
+
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                sockets.ForEach(s => s.Send(img));
+                input = Console.ReadLine();
+            }
+        }
+
+    }
+}
